c0d6c1b3f155ce6960dbd80957b58ec24a7e89dc
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2022-10-06 18:18:31 +0600

    new enemy variant created.

--- Plane Shooter/Assets/Scripts/MainGameScripts/Player/Shooting.cs
+++ Plane Shooter/Assets/Scripts/MainGameScripts/Player/Shooting.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
@@ -7,31 +6,29 @@ public class Shooting : MonoBehaviour
     [SerializeField] private float shootRepeatTime = 0.3f;
     [SerializeField] private GameObject bulletPrefab;
     
-    [SerializeField] private Transform spawnPoint1;
-    [SerializeField] private Transform spawnPoint2;
-    [SerializeField] private GameObject muzzleFlash;
-    [SerializeField] private GameObject muzzleFlash2;
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private GameObject[] muzzleFlashs;
+    
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Shoot());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private IEnumerator Shoot()
     {
-        Instantiate(bulletPrefab, spawnPoint1.position, Quaternion.identity);
-        Instantiate(bulletPrefab, spawnPoint2.position, Quaternion.identity);
-        muzzleFlash.SetActive(true);
-        muzzleFlash2.SetActive(true);
-        yield return new WaitForSeconds(0.05f);
-        muzzleFlash.SetActive(false);
-        muzzleFlash2.SetActive(false);
+        // Spawn bullet
+        foreach (var spawnPoint in spawnPoints) Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity);
+        
+        // Spawn muzzle flash
+        foreach (var muzzleFlash in muzzleFlashs) muzzleFlash.SetActive(true);
+        
+        
+        // Wait for a bit and then disable muzzle flash
+        yield return new WaitForSeconds(0.09f);
+        foreach (var muzzleFlash in muzzleFlashs) muzzleFlash.SetActive(false);
+        
+        // Wait for a bit and then repeat
         yield return new WaitForSeconds(shootRepeatTime);
         StartCoroutine(Shoot());
     }
